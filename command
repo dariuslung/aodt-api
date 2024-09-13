cc9e0a10a9f9737000857d2c61c35d03098ccf1b
dariuslung <dariuslung@hotmail.com>
2024-09-13 10:05:41 +0800

    Run service with run.sh

--- command
+++
@@ -1,1 +0,0 @@
-./kit --exec test/api.py --enable omni.kit.asset_converter --enable omni.services.core   --enable omni.services.transport.server.http --/exts/omni.kit.registry.nucleus/registries/0/name=kit/services --/exts/omni.kit.registry.nucleus/registries/0/url=https://dw290v42wisod.cloudfront.net/exts/kit/serviceses 
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../kit --exec api.py --enable omni.kit.asset_converter --enable omni.services.core   --enable omni.services.transport.server.http --/exts/omni.kit.registry.nucleus/registries/0/name=kit/services --/exts/omni.kit.registry.nucleus/registries/0/url=https://dw290v42wisod.cloudfront.net/exts/kit/serviceses 
